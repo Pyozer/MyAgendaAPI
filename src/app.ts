ad64e84407398dba69429eb22b19e51b192af636
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-10-14 16:38:40 +0200

    Add memjs to improve performance, and reduce university flooding

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { config as dotEnvConfig } from "dotenv"
-import express, { Application } from "express"
+import express, { Application, Response, NextFunction, Request } from "express"
+import memjs from 'memjs'
 import middlewares from "./middlewares"
 import baseRoute from "./routes/base"
 import { welcome } from "./routes/welcome"
@@ -9,10 +10,30 @@ dotEnvConfig()
 
 const app: Application = express()
 
+const { PORT = 3000, MEMCACHIER_SERVERS } = process.env
+let client = memjs.Client.create(MEMCACHIER_SERVERS, {
+    expires: 30
+})
+
 applyMiddlewares(middlewares, app)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const { url: key } = req
+    client.get(key, (err, result) => {
+        if (err == null && result != null) {
+            res.send(result.toString())
+        } else {
+            let oldSend = res.send;
+            res.send = function (body?: any): Response {
+                client.set(key, `${body}`, {})
+                return oldSend.apply(res, arguments);
+            }
+            next()
+        }
+    })
+})
 
 app.use("/api", baseRoute)
 app.use("/", welcome)
 
-const { PORT = 3000 } = process.env
 app.listen(PORT, () => console.log(`API listening on port ${PORT}`))
