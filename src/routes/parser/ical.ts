a7bc200da60b9ed48eef2177c3dfda5dab220ffb
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-10-16 17:27:49 +0200

    Fix API bug on some ICAL format

--- src/routes/parser/ical.ts
+++ src/routes/parser/ical.ts
@@ -5,9 +5,14 @@ const ical = require('node-ical');
 const icalFromUrl = async (url: string) => {
     const icalRaw: string = await request.get(url, {
         followAllRedirects: true
-    })    
+    })
     const icalData = await ical.parseICS(icalRaw);
-    
+
+    const getValue = (obj: any, value: string) => {
+        if (!obj[value]) return ''
+        return obj[value].val || obj[value]
+    }
+
     const vevents = []
     for (const key in icalData) {
         if (icalData.hasOwnProperty(key)) {
@@ -17,9 +22,9 @@ const icalFromUrl = async (url: string) => {
                     uid: key,
                     dtstart: ev.start,
                     dtend: ev.end,
-                    description: ev.description || '',
-                    location: ev.location || '',
-                    summary: ev.summary,
+                    description: getValue(ev, 'description'),
+                    location: getValue(ev, 'location'),
+                    summary: getValue(ev, 'summary'),
                     dtstamp: ev.dtstamp,
                     created: ev.created,
                     lastmodified: ev.lastmodified,
